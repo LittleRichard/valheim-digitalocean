8db9e8ad0200d2cb11a74609ac15bf28c856bdcf
AD <anson.dorsey@gmail.com>
2021-04-24 23:12:55 -0400

    start/stop server works, but not proven yet
    i think the current image got screwed along the way,
    but if started from scratch i think it'll work.

--- client/start_server.py
+++ client/start_server.py
@@ -1,42 +1,99 @@
+import time
+
 import digitalocean
 import yaml
+from paramiko import SSHClient, AutoAddPolicy
 
 with open('config.yml') as file:
     config_data = yaml.full_load(file)
 
-DO_TOKEN = config_data.get('API_KEY')
-assert DO_TOKEN, 'no digital ocean API key specified'
+API_KEY = config_data.get('api_key')
+assert API_KEY, 'no digital ocean API key specified'
 
-SNAP_NAME = config_data.get('snapshot_name')
-assert SNAP_NAME, 'no snap name specified'
+LOCAL_SSH_PASSWORD = config_data.get('local_ssh_password')
+assert LOCAL_SSH_PASSWORD, 'no local SSH password'
 
-manager = digitalocean.Manager(token=DO_TOKEN)
+IMAGE_BASE_NAME = config_data.get('snapshot_name')
+assert IMAGE_BASE_NAME, 'no snap name specified'
+
+SIZE_SLUG = config_data.get('size_slug')
+assert SIZE_SLUG, 'no size slug specified'
+
+REGION = config_data.get('region')
+assert REGION, 'no region specified'
+
+manager = digitalocean.Manager(token=API_KEY)
 
 valheim_droplet = None
 for droplet in manager.get_all_droplets():
     print(f'Scanning {droplet.name:40s} | {droplet.ip_address:16s} | {droplet.status}')
 
-    if droplet.name == SNAP_NAME:
+    if droplet.name.startswith(IMAGE_BASE_NAME):
         valheim_droplet = droplet
 
 if valheim_droplet is None:
     # go find the snapshot and make a droplet for it
     snapshots = manager.get_droplet_snapshots()
-    for snap in snapshots:
-        if snap.name == SNAP_NAME:
-            valheim_snap = snap
-            break
-    else:
-        assert False, f'could not find snapshot {SNAP_NAME}'
+    valheim_snap = None
+    for snap in sorted(snapshots, key=lambda x: x.created_at):
+        if snap.name.startswith(IMAGE_BASE_NAME):
+            print(f'found snap {snap.name} created at {snap.created_at}')
+            valheim_snap = snap  # will end with the newest one
 
-    print('no droplet, but found snap. creating from snap')
+    if valheim_snap is None:
+        assert False, f'could not find snapshot {IMAGE_BASE_NAME}'
 
-    valheim_droplet = digitalocean.Droplet(token=DO_TOKEN,
-                                           name=SNAP_NAME,
-                                           # region='nyc2', # New York 2
-                                           image=snap.id, # Ubuntu 20.04 x64
-                                           # size_slug='s-1vcpu-1gb',  # 1GB RAM, 1 vCPU
-                                           backups=False)
+    print(f'no droplet, but found snap. creating from snap {snap.name}')
+
+    valheim_droplet = digitalocean.Droplet(token=API_KEY,
+                                           name=snap.name,
+                                           region=REGION,
+                                           image=snap.id,
+                                           size_slug=SIZE_SLUG,
+                                           backups=False,
+                                           ssh_keys=manager.get_all_sshkeys())
     valheim_droplet.create()
-    print('Done creating, waiting for start')
+    idx = 0
+    max_idx = 100
+    sleep_sec = 5
+    while True:
+        valheim_droplet.load()
+        print(f'Status {valheim_droplet.status} | {valheim_droplet.ip_address} '
+              f'after {idx * sleep_sec} sec')
 
+        if valheim_droplet.status == 'active':
+            time.sleep(sleep_sec)  # give it a second to become ready
+            break
+        idx += 1
+        time.sleep(sleep_sec)
+
+        assert idx != max_idx, 'hit max retries... ruh roh'
+else:
+    print(f'Droplet already exists at {valheim_droplet.ip_address}, '
+          f'not creating one.')
+
+print('Droplet is ready, starting up valheim server. '
+      'This may take several minutes...')
+ssh_client = SSHClient()
+ssh_client.load_system_host_keys()
+ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+ssh_client.connect(passphrase=LOCAL_SSH_PASSWORD,
+                   hostname=valheim_droplet.ip_address,
+                   username='root')
+try:
+    command = """
+    cd valheim-digitalocean && \
+    git pull && \
+    cd server && \
+    docker system prune --force && \
+    docker-compose up -d && \
+    sleep 10 && \
+    docker exec -i valheim bash -c "cd ../valheim ; odin start"
+    """
+
+    (stdin, stdout, stderr) = ssh_client.exec_command(command)
+    print(''.join(stdout.readlines()))
+finally:
+    ssh_client.close()
+
+print(f'\n**\nVIKING TIME at IP: {valheim_droplet.ip_address}\n**\n')
---
+++ client/stop_server.py
@@ -0,0 +1,77 @@
+import time
+
+import digitalocean
+import yaml
+from paramiko import SSHClient, AutoAddPolicy
+
+with open('config.yml') as file:
+    config_data = yaml.full_load(file)
+
+API_KEY = config_data.get('api_key')
+assert API_KEY, 'no digital ocean API key specified'
+
+IMAGE_BASE_NAME = config_data.get('snapshot_name')
+assert IMAGE_BASE_NAME, 'no snap name specified'
+
+LOCAL_SSH_PASSWORD = config_data.get('local_ssh_password')
+assert LOCAL_SSH_PASSWORD, 'no local SSH password'
+
+SNAPS_TO_KEEP = config_data.get('snaps_to_keep')
+assert SNAPS_TO_KEEP, 'must keep 1 snap'
+
+manager = digitalocean.Manager(token=API_KEY)
+
+valheim_droplet = None
+for droplet in manager.get_all_droplets():
+    print(f'Scanning {droplet.name:40s} | {droplet.ip_address:16s} | {droplet.status}')
+
+    if droplet.name.startswith(IMAGE_BASE_NAME) and droplet.status == 'active':
+        valheim_droplet = droplet
+
+if valheim_droplet is None:
+    print('No droplet found, nothing to stop.  Exiting')
+    exit(0)
+
+ssh_client = SSHClient()
+ssh_client.load_system_host_keys()
+ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+ssh_client.connect(passphrase=LOCAL_SSH_PASSWORD,
+                   hostname=valheim_droplet.ip_address,
+                   username='root')
+
+command = """
+cd valheim-digitalocean && \
+docker exec -i valheim bash -c "cd ../valheim ; odin stop" && \
+sleep 10 && \
+docker-compose down
+"""
+
+(stdin, stdout, stderr) = ssh_client.exec_command(command)
+print('\nserver stdout (wait for it to finish)')
+print(''.join(stdout.readlines()))
+
+print('snapshot is processing, cleaning up old snapshots')
+snapshots = sorted(
+    (x for x in manager.get_droplet_snapshots()
+     if x.name.startswith(IMAGE_BASE_NAME)),
+    key=lambda x: x.created_at
+)
+print(f'Found {len(snapshots)} snapshots')
+if len(snapshots) > SNAPS_TO_KEEP:
+    for snap in snapshots[:-1*SNAPS_TO_KEEP]:
+        print(f'destroying snapshot {snap}')
+        snap.destroy()
+
+print('droplet powering down to take snapshot. '
+      'This will take a while...')
+snap_name = IMAGE_BASE_NAME + '-' + str(int(time.time()))
+snap_action = valheim_droplet.take_snapshot(snap_name,
+                                            return_dict=False,
+                                            power_off=True)
+snap_action.wait()
+
+valheim_droplet.load()
+print(f'finished snapshot, droplet status: {valheim_droplet.status}')
+
+print('Destroying droplet')
+valheim_droplet.destroy()
